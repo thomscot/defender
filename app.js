4a180e9660ed64389c3cfa22eb54cf412a4b8603
Tommy <tommaso.sct@gmail.com>
2017-07-05 00:35:54 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies (DEFAULT)
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+  
+/**
+ * Module dependencies (ADDED)
+ */
+// Serves latest bootstrap version to express apps 
+var bootstrap = require('express-bootstrap-service');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+// for bootstrap
+app.use(bootstrap.serve);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});

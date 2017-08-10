ed2147894d6f54e45ba962ed2f5edc6bb52c6c29
Tommy <tommaso.sct@gmail.com>
2017-08-10 07:20:58 +0000

    implemented check confirm_email=email

--- routes/members.js
+++ routes/members.js
@@ -59,19 +59,24 @@ router.post('/addmember', validator, async (req,res) => {
  * Middleware validator
  */
 function validator(req, res, next) {
+
   req.checkBody('namekanji', 'お名前[漢字]をご入力下さい。').notEmpty();
   req.checkBody('namekana', 'お名前[かな]をご入力下さい。').notEmpty();
-  req.checkBody('email', '入力されたメールが正しくありません。').isEmail();
+  req.checkBody('email', '入力された電子郵便が正しくありません。').isEmail();
+  req.checkBody('email_confirm', '入力された確認電子郵便が正しくありません。').equals(req.body.email)
   req.checkBody('location', '位置をご入力下さい。').notEmpty();
   req.checkBody('age', '年齢をご入力下さい。').notEmpty();
   req.checkBody('sex', '性別をご入力下さい。').notEmpty();
+  
   //TODO: req.validationErrors() may be removed in a future version. Use req.getValidationResult().
   var errors = req.validationErrors();
+  
   if (errors) {
-    console.log(errors) 
     res.json({msg: 'validation', errors:errors}); // send back the errors as json
   }
   else {
+    // If validation pass, I don't want to insert the email twice in the DB
+    delete req.body.email_confirm
     next();
   }
 };

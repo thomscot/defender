04129e8e457c131d1821dabbe51db4a53b6e2eb5
Tommy <tommaso.sct@gmail.com>
2017-08-22 06:25:55 +0000

    added manifesto download feature

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+
 /*
  * GET home page.
  */
 router.get('/', function(req, res) {
   res.render('index');
 });
+
+
+/*
+ * Download the manifesto.
+ */
+router.get('/gekibun_download', function (req, res, next) {
+    var filePath = __dirname +　"/../public/data/gekibun.txt"; 
+    var fileName = "gekibun.txt"; 
+    res.download(filePath, fileName);    
+});
+
 
 module.exports = router;
